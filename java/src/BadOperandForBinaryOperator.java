d23b1d00ec76401e0ce3fd62280809adcb80ef1f
Sudharshana L <ls@vmware.com>
2018-11-04 17:50:26 +0530

    coding problem and error explanation

---
+++ java/src/BadOperandForBinaryOperator.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ls on 11/4/2018.
+ */
+public class BadOperandForBinaryOperator {
+    public static void main(String[] args) {
+        System.out.println("Error occurs here - " + (2 - 3));
+        System.out.println("Error occurs here again - " + (2^8));
+    }
+}
